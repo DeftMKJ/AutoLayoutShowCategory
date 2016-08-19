e22e3380e2ba35978152b81af5138db3e5a23834
Deft_MKJing <mkjing@mkjingdemac-mini.local>
2016-08-19 10:56:52 +0800

    Initial Commit

---
+++ AutoLayoutShowTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutShowTime
+//
+//  Created by MKJING on 16/8/19.
+//  Copyright © 2016年 MKJING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
